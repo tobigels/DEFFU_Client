ab87446fce00ee9477e28df3358b70eb2db6ebc1
DESKTOP-RIMBG1A\mindQ <tobigels@mindq.de>
2018-02-09 17:58:12 +0100

    AP 3.4 Changed primitives to Instantiating gameobjects

--- Assets/Scripts/AvatarConnector_IN.cs
+++ Assets/Scripts/AvatarConnector_IN.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
+using System.Collections;
 
-public class AvatarConnector_IN {
+public class AvatarConnector_IN : MonoBehaviour {
 
     #region FIELDS
 
     private GameObject leftController;
     private GameObject rightController;
     private GameObject hmd;
+
+    public GameObject _leftController;
+    public GameObject _rightController;
+    public GameObject _hmd;
 
     #endregion
 
@@ -20,12 +25,32 @@ public class AvatarConnector_IN {
 
         //Instantiate Avatar with player
 
-        hmd = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        hmd.name = playerName;
+        GameObject player = new GameObject();
+        player.name = playerName;
+
+        hmd = (GameObject) Instantiate(_hmd);
+        hmd.name = "hmd";
+        hmd.transform.parent = player.transform;
+
+        leftController = (GameObject)Instantiate(_leftController);
+        leftController.name = "leftController";
+        leftController.transform.parent = player.transform;
+
+        rightController = (GameObject)Instantiate(_rightController);
+        rightController.name = "rightController";
+        rightController.transform.parent = player.transform;
+
     }
 
     public void UpdataAvatarConnector(InputFrame inputFrame) {
         hmd.transform.position = inputFrame.hmd_pos;
+        hmd.transform.rotation = inputFrame.hmd_rot;
+
+        rightController.transform.position = inputFrame.controller_right_pos;
+        rightController.transform.rotation = inputFrame.controller_right_rot;
+
+        leftController.transform.position = inputFrame.controller_left_pos;
+        leftController.transform.rotation = inputFrame.controller_left_rot;
     }
 
     #endregion
