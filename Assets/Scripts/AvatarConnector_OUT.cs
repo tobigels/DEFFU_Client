0d90d79b6dd336430a3fe4b07e137c1a41362dc4
DESKTOP-RIMBG1A\mindQ <tobigels@mindq.de>
2018-02-13 13:49:33 +0100

    AP 3.4 enhanced InputFrame to support Button-Events

--- Assets/Scripts/AvatarConnector_OUT.cs
+++ Assets/Scripts/AvatarConnector_OUT.cs
@@ -7,8 +7,17 @@ internal class AvatarConnector_OUT {
     #region FIELDS
 
     private Transform headsetTransform;
-    private Transform leftControllerTransform;
-    private Transform rightControllerTransform;
+
+    private GameObject leftController;
+    private GameObject rightController;
+
+    private SteamVR_TrackedObject leftController_tracked;
+    private SteamVR_TrackedObject rightController_tracked;
+
+    private SteamVR_Controller.Device leftController_device;
+    private SteamVR_Controller.Device rightController_device;
+
+    private InputFrame previousFrame;
 
     #endregion
 
@@ -20,8 +29,9 @@ internal class AvatarConnector_OUT {
 
     public AvatarConnector_OUT() {
         headsetTransform = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.Headset);
-        leftControllerTransform = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.LeftController);
-        rightControllerTransform = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.RightController);
+        leftController = VRTK_DeviceFinder.GetControllerLeftHand();
+        rightController = VRTK_DeviceFinder.GetControllerRightHand();
+        previousFrame = new InputFrame();
     }
 
     /// <summary>
@@ -29,7 +39,7 @@ internal class AvatarConnector_OUT {
     /// </summary>
     /// <returns></returns>
     public InputFrame getInput() {
-        InputFrame inputFrame = new InputFrame();
+        InputFrame inputFrame = new InputFrame(previousFrame);
 
         if (headsetTransform != null) {
             inputFrame.hmd_pos = headsetTransform.position;
@@ -38,18 +48,54 @@ internal class AvatarConnector_OUT {
             headsetTransform = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.Headset);
         }
 
-        if (leftControllerTransform != null) {
-            inputFrame.controller_left_pos = leftControllerTransform.position;
-            inputFrame.controller_left_rot = leftControllerTransform.rotation;
+        if (leftController != null) {
+            inputFrame.controller_left_pos = leftController.transform.position;
+            inputFrame.controller_left_rot = leftController.transform.rotation;
+
+            leftController_tracked = leftController.GetComponent<SteamVR_TrackedObject>();
+            leftController_device = SteamVR_Controller.Input((int)leftController_tracked.index);
+
+            //
+            inputFrame.ButtonThree_pushed = leftController_device.GetPress(Valve.VR.EVRButtonId.k_EButton_A);
+            inputFrame.ButtonThree_touched = (inputFrame.ButtonThree_pushed ? true : leftController_device.GetTouch(Valve.VR.EVRButtonId.k_EButton_A));
+            inputFrame.ButtonFour_pushed = leftController_device.GetPress(Valve.VR.EVRButtonId.k_EButton_ApplicationMenu);
+            inputFrame.ButtonFour_touched = (inputFrame.ButtonFour_pushed ? true : leftController_device.GetTouch(Valve.VR.EVRButtonId.k_EButton_ApplicationMenu));
+            inputFrame.PrimaryIndexTrigger_pushed = leftController_device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger);
+            inputFrame.PrimaryIndexTrigger_touched = (inputFrame.PrimaryIndexTrigger_pushed ? true : leftController_device.GetTouch(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger));
+            inputFrame.PrimaryHandTrigger_pushed = leftController_device.GetPress(Valve.VR.EVRButtonId.k_EButton_Axis0);
+            inputFrame.PrimaryHandTrigger_touched = (inputFrame.PrimaryHandTrigger_pushed ? true : leftController_device.GetTouch(Valve.VR.EVRButtonId.k_EButton_Axis0));
+            inputFrame.PrimaryThumbstick_pushed = leftController_device.GetPress(Valve.VR.EVRButtonId.k_EButton_Axis0);
+            inputFrame.PrimaryThumbstick_touched = (inputFrame.PrimaryThumbstick_pushed ? true : leftController_device.GetTouch(Valve.VR.EVRButtonId.k_EButton_Axis0));
+            //
+
+            inputFrame.PrimaryThumbstick_directionX = leftController_device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0).x;
+            inputFrame.PrimaryThumbstick_directionY = leftController_device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0).y;
         } else {
-            leftControllerTransform = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.LeftController);
+            leftController = VRTK_DeviceFinder.GetControllerLeftHand();
         }
 
-        if (rightControllerTransform != null) {
-            inputFrame.controller_right_pos = rightControllerTransform.position;
-            inputFrame.controller_right_rot = rightControllerTransform.rotation;
+        if (rightController != null) {
+            inputFrame.controller_right_pos = rightController.transform.position;
+            inputFrame.controller_right_rot = leftController.transform.rotation;
+
+            rightController_tracked = rightController.GetComponent<SteamVR_TrackedObject>();
+            rightController_device = SteamVR_Controller.Input((int)rightController_tracked.index);
+
+            inputFrame.ButtonOne_pushed = rightController_device.GetPressDown(Valve.VR.EVRButtonId.k_EButton_A);
+            inputFrame.ButtonOne_touched = (inputFrame.ButtonOne_pushed ? true : rightController_device.GetTouch(Valve.VR.EVRButtonId.k_EButton_A));
+            inputFrame.ButtonTwo_pushed = rightController_device.GetPress(Valve.VR.EVRButtonId.k_EButton_ApplicationMenu);
+            inputFrame.ButtonTwo_touched = (inputFrame.ButtonTwo_pushed ? true : rightController_device.GetTouch(Valve.VR.EVRButtonId.k_EButton_ApplicationMenu));
+            inputFrame.SecondaryIndexTrigger_pushed = rightController_device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger);
+            inputFrame.SecondaryIndexTrigger_touched = (inputFrame.SecondaryIndexTrigger_pushed ? true : rightController_device.GetTouch(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger));
+            inputFrame.SecondaryHandTrigger_pushed = rightController_device.GetPress(Valve.VR.EVRButtonId.k_EButton_Axis0);
+            inputFrame.SecondaryHandTrigger_touched = (inputFrame.SecondaryHandTrigger_pushed ? true : rightController_device.GetTouch(Valve.VR.EVRButtonId.k_EButton_Axis0));
+            inputFrame.SecondaryThumbstick_pushed = rightController_device.GetPress(Valve.VR.EVRButtonId.k_EButton_Axis0);
+            inputFrame.SecondaryThumbstick_touched = (inputFrame.SecondaryThumbstick_pushed ? true : rightController_device.GetTouch(Valve.VR.EVRButtonId.k_EButton_Axis0));
+
+            inputFrame.SecondaryThumbstick_directionX = rightController_device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0).x;
+            inputFrame.SecondaryThumbstick_directionY = rightController_device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0).y;
         } else {
-            rightControllerTransform = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.RightController);
+            rightController = VRTK_DeviceFinder.GetControllerRightHand();
         }
 
         return inputFrame;
--- Assets/Scripts/InputFrame.cs
+++ Assets/Scripts/InputFrame.cs
@@ -10,4 +10,80 @@ public class InputFrame {
     public Quaternion controller_left_rot;
     public Vector3 hmd_pos;
     public Quaternion hmd_rot;
+
+    public bool ButtonOne_pushed;
+    public bool ButtonOne_touched;
+    public bool ButtonTwo_pushed;
+    public bool ButtonTwo_touched;
+    public bool ButtonThree_pushed;
+    public bool ButtonThree_touched;
+    public bool ButtonFour_pushed;
+    public bool ButtonFour_touched;
+
+    //primary = left hand
+    public bool PrimaryIndexTrigger_pushed;
+    public bool PrimaryIndexTrigger_touched;
+    public bool PrimaryHandTrigger_pushed;
+    public bool PrimaryHandTrigger_touched;
+    public bool PrimaryThumbstick_pushed;
+    public bool PrimaryThumbstick_touched;
+
+    //secondary = right hand
+    public bool SecondaryIndexTrigger_pushed;
+    public bool SecondaryIndexTrigger_touched;
+    public bool SecondaryHandTrigger_pushed;
+    public bool SecondaryHandTrigger_touched;
+    public bool SecondaryThumbstick_pushed;
+    public bool SecondaryThumbstick_touched;
+
+    public float PrimaryThumbstick_directionX;
+    public float PrimaryThumbstick_directionY;
+    public float SecondaryThumbstick_directionX;
+    public float SecondaryThumbstick_directionY;
+
+    public InputFrame() {
+        this.ButtonOne_pushed = false;
+        this.ButtonOne_touched = false;
+        this.ButtonTwo_pushed = false;
+        this.ButtonTwo_touched = false;
+        this.ButtonThree_pushed = false;
+        this.ButtonThree_touched = false;
+        this.ButtonFour_pushed = false;
+        this.ButtonFour_touched = false;
+        this.PrimaryIndexTrigger_pushed = false;
+        this.PrimaryIndexTrigger_touched = false;
+        this.PrimaryHandTrigger_pushed = false;
+        this.PrimaryHandTrigger_touched = false;
+        this.PrimaryThumbstick_pushed = false;
+        this.PrimaryThumbstick_touched = false;
+        this.SecondaryIndexTrigger_pushed = false;
+        this.SecondaryIndexTrigger_touched = false;
+        this.SecondaryHandTrigger_pushed = false;
+        this.SecondaryHandTrigger_touched = false;
+        this.SecondaryThumbstick_pushed = false;
+        this.SecondaryThumbstick_touched = false;   
+    }
+
+    public InputFrame(InputFrame nFrame) {
+        this.ButtonOne_pushed = nFrame.ButtonOne_pushed;
+        this.ButtonOne_touched = nFrame.ButtonOne_touched;
+        this.ButtonTwo_pushed = nFrame.ButtonTwo_pushed;
+        this.ButtonTwo_touched = nFrame.ButtonTwo_touched;
+        this.ButtonThree_pushed = nFrame.ButtonThree_pushed;
+        this.ButtonThree_touched = nFrame.ButtonThree_touched;
+        this.ButtonFour_pushed = nFrame.ButtonFour_pushed;
+        this.ButtonFour_touched = nFrame.ButtonFour_touched;
+        this.PrimaryIndexTrigger_pushed = nFrame.PrimaryIndexTrigger_pushed;
+        this.PrimaryIndexTrigger_touched = nFrame.PrimaryIndexTrigger_touched;
+        this.PrimaryHandTrigger_pushed = nFrame.PrimaryHandTrigger_pushed;
+        this.PrimaryHandTrigger_touched = nFrame.PrimaryHandTrigger_touched;
+        this.PrimaryThumbstick_pushed = nFrame.PrimaryThumbstick_pushed;
+        this.PrimaryThumbstick_touched = nFrame.PrimaryThumbstick_touched;
+        this.SecondaryIndexTrigger_pushed = nFrame.SecondaryIndexTrigger_pushed;
+        this.SecondaryIndexTrigger_touched = nFrame.SecondaryIndexTrigger_touched;
+        this.SecondaryHandTrigger_pushed = nFrame.SecondaryHandTrigger_pushed;
+        this.SecondaryHandTrigger_touched = nFrame.SecondaryHandTrigger_touched;
+        this.SecondaryThumbstick_pushed = nFrame.SecondaryThumbstick_pushed;
+        this.SecondaryThumbstick_touched = nFrame.SecondaryThumbstick_touched;
+    }
 }
