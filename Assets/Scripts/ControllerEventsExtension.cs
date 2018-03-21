7fbee22bd432f34f32ddf24a4a6b10b2b7d6b35b
DESKTOP-RIMBG1A\mindQ <tobigels@mindq.de>
2018-03-22 00:25:11 +0100

    AP 3.4 More Debug-Methods

--- Assets/Scripts/ControllerEventsExtension.cs
+++ Assets/Scripts/ControllerEventsExtension.cs
@@ -18,7 +18,11 @@ public class ControllerEventsExtension : VRTK_ControllerEvents {
     #region METHODS
 
     // --------------------------------------- Private methods ---------------------------------------
-
+    
+    private void TestHelper(string name) {
+        Debug.Log(name + PlayerManager.instance.globalTime + " I " + PlayerManager.instance.GameTurn + " I " + gameObject.name);
+    }
+        
     // --------------------------------------- Public methods ---------------------------------------
 
     public AvatarConnector_IN AvatarConnector {
@@ -53,12 +57,12 @@ public class ControllerEventsExtension : VRTK_ControllerEvents {
         if (buttonPush[0] != buttonPush_previous[0]) {
             if(buttonPush[0]) {
                 //ButtonOne Pressed
-                Debug.Log("Button One pressed " + PlayerManager.instance.globalTime + " I " + PlayerManager.instance.GameTurn);
+                TestHelper("Button One/Three pressed ");
                 OnButtonOnePressed(SetControllerEvent(ref buttonOnePressed, true, 1f));
                 EmitAlias(ButtonAlias.ButtonOnePress, true, 1f, ref buttonOnePressed);
             } else {
                 //ButtonOne Released
-                Debug.Log("Button One released " + PlayerManager.instance.globalTime + " I " + PlayerManager.instance.GameTurn);
+                TestHelper("Button One/Three released ");
                 OnButtonOneReleased(SetControllerEvent(ref buttonOnePressed, false, 0f));
                 EmitAlias(ButtonAlias.ButtonOnePress, false, 0f, ref buttonOnePressed);
             }
@@ -67,12 +71,12 @@ public class ControllerEventsExtension : VRTK_ControllerEvents {
         if (buttonPush[1] != buttonPush_previous[1]) {
             if (buttonPush[1]) {
                 //ButtonTwo Pressed
-                Debug.Log("Button Two pressed " + PlayerManager.instance.globalTime + " I " + PlayerManager.instance.GameTurn);
+                TestHelper("Button Two/Four pressed ");
                 OnButtonTwoPressed(SetControllerEvent(ref buttonTwoPressed, true, 1f));
                 EmitAlias(ButtonAlias.ButtonTwoPress, true, 1f, ref buttonTwoPressed);
             } else {
                 //ButtonTwo Released
-                Debug.Log("Button Two released " + PlayerManager.instance.globalTime + " I " + PlayerManager.instance.GameTurn);
+                TestHelper("Button Two/Four released ");
                 OnButtonTwoReleased(SetControllerEvent(ref buttonTwoPressed, false, 0f));
                 EmitAlias(ButtonAlias.ButtonTwoPress, false, 0f, ref buttonTwoPressed);
             }
@@ -81,12 +85,12 @@ public class ControllerEventsExtension : VRTK_ControllerEvents {
         if (buttonPush[2] != buttonPush_previous[2]) {
             if (buttonPush[2]) {
                 //Trigger Pressed
-                Debug.Log("Trigger pressed " + PlayerManager.instance.globalTime + " I " + PlayerManager.instance.GameTurn);
+                TestHelper("Trigger pressed ");
                 OnTriggerPressed(SetControllerEvent(ref triggerPressed, true, 0.0f));
                 EmitAlias(ButtonAlias.TriggerPress, true, 0.0f, ref triggerPressed);
             } else {
                 // Trigger Pressed end
-                Debug.Log("Trigger released " + PlayerManager.instance.globalTime);
+                TestHelper("Trigger released ");
                 OnTriggerReleased(SetControllerEvent(ref triggerPressed, false, 0f));
                 EmitAlias(ButtonAlias.TriggerPress, false, 0f, ref triggerPressed);
             }
@@ -95,12 +99,12 @@ public class ControllerEventsExtension : VRTK_ControllerEvents {
         if (buttonPush[3] != buttonPush_previous[3]) {
             if (buttonPush[3]) {
                 //Grip Pressed
-                Debug.Log("Grip pressed " + PlayerManager.instance.globalTime + " I " + PlayerManager.instance.GameTurn);
+                TestHelper("HandTrigger pressed ");
                 OnGripPressed(SetControllerEvent(ref gripPressed, true, 0.0f));
                 EmitAlias(ButtonAlias.GripPress, true, 0.0f, ref gripPressed);
             } else {
                 //Grip Pressed End
-                Debug.Log("Grip released " + PlayerManager.instance.globalTime);
+                TestHelper("HandTrigger released ");
                 OnGripReleased(SetControllerEvent(ref gripPressed, false, 0f));
                 EmitAlias(ButtonAlias.GripPress, false, 0f, ref gripPressed);
             }
@@ -109,12 +113,12 @@ public class ControllerEventsExtension : VRTK_ControllerEvents {
         if (buttonPush[4] != buttonPush_previous[4]) {
             if (buttonPush[4]) {
                 //Touchpad Pressed
-                Debug.Log("Thumbstick pressed " + PlayerManager.instance.globalTime + " I " + PlayerManager.instance.GameTurn);
+                TestHelper("Thumbstick pressed ");
                 OnTouchpadPressed(SetControllerEvent(ref touchpadPressed, true, 1f));
                 EmitAlias(ButtonAlias.TouchpadPress, true, 1f, ref touchpadPressed);
             } else {
                 //Touchpad Released
-                Debug.Log("Thumbstick released " + PlayerManager.instance.globalTime + " I " + PlayerManager.instance.GameTurn);
+                TestHelper("Thumbstick released ");
                 OnTouchpadReleased(SetControllerEvent(ref touchpadPressed, false, 0f));
                 EmitAlias(ButtonAlias.TouchpadPress, false, 0f, ref touchpadPressed);
             }
@@ -123,10 +127,12 @@ public class ControllerEventsExtension : VRTK_ControllerEvents {
         if(buttonTouch[0] != buttonTouch_previous[0]) {
             if(buttonTouch[0]) {
                 //ButtonOne Touched
+                TestHelper("Button One/Three touched");
                 OnButtonOneTouchStart(SetControllerEvent(ref buttonOneTouched, true, 1f));
                 EmitAlias(ButtonAlias.ButtonOneTouch, true, 1f, ref buttonOneTouched);
             } else {
                 //ButtonOne Touched End
+                TestHelper("Button One/Three untouched");
                 OnButtonOneTouchEnd(SetControllerEvent(ref buttonOneTouched, false, 0f));
                 EmitAlias(ButtonAlias.ButtonOneTouch, false, 0f, ref buttonOneTouched);
             }
@@ -135,10 +141,12 @@ public class ControllerEventsExtension : VRTK_ControllerEvents {
         if (buttonTouch[1] != buttonTouch_previous[1]) {
             if (buttonTouch[1]) {
                 //ButtonTwo Touched
+                TestHelper("Button Two/Four touched");
                 OnButtonTwoTouchStart(SetControllerEvent(ref buttonTwoTouched, true, 1f));
                 EmitAlias(ButtonAlias.ButtonTwoTouch, true, 1f, ref buttonTwoTouched);
             } else {
                 //ButtonTwo Touched End
+                TestHelper("Button Two/Four untouched");
                 OnButtonTwoTouchEnd(SetControllerEvent(ref buttonTwoTouched, false, 0f));
                 EmitAlias(ButtonAlias.ButtonTwoTouch, false, 0f, ref buttonTwoTouched);
             }
@@ -147,10 +155,12 @@ public class ControllerEventsExtension : VRTK_ControllerEvents {
         if (buttonTouch[2] != buttonTouch_previous[2]) {
             if (buttonTouch[2]) {
                 //Trigger Touched
+                TestHelper("Trigger touched");
                 OnTriggerTouchStart(SetControllerEvent(ref triggerTouched, true, 0.0f));
                 EmitAlias(ButtonAlias.TriggerTouch, true, 0.0f, ref triggerTouched);
             } else {
                 //Trigger Touch End
+                TestHelper("Trigger untouched");
                 OnTriggerTouchEnd(SetControllerEvent(ref triggerTouched, false, 0f));
                 EmitAlias(ButtonAlias.TriggerTouch, false, 0f, ref triggerTouched);
             }
@@ -159,10 +169,12 @@ public class ControllerEventsExtension : VRTK_ControllerEvents {
         if (buttonTouch[3] != buttonTouch_previous[3]) {
             if (buttonTouch[3]) {
                 //Grip Touched
+                TestHelper("HandTrigger touched");
                 OnGripTouchStart(SetControllerEvent(ref gripTouched, true, 0.0f));
                 EmitAlias(ButtonAlias.GripTouch, true, 0.0f, ref gripTouched);
             } else {
                 // Grip Touch End
+                TestHelper("HandTrigger untouched");
                 OnGripTouchEnd(SetControllerEvent(ref gripTouched, false, 0f));
                 EmitAlias(ButtonAlias.GripTouch, false, 0f, ref gripTouched);
             }
@@ -171,10 +183,12 @@ public class ControllerEventsExtension : VRTK_ControllerEvents {
         if (buttonTouch[4] != buttonTouch_previous[4]) {
             if (buttonTouch[4]) {
                 //Touchpad Touched
+                TestHelper("Thumbstick touched");
                 OnTouchpadTouchStart(SetControllerEvent(ref touchpadTouched, true, 1f));
                 EmitAlias(ButtonAlias.TouchpadTouch, true, 1f, ref touchpadTouched);
             } else {
                 //Touchpad Untouched
+                TestHelper("Thumbstick untouched");
                 OnTouchpadTouchEnd(SetControllerEvent(ref touchpadTouched, false, 0f));
                 EmitAlias(ButtonAlias.TouchpadTouch, false, 0f, ref touchpadTouched);
                 touchpadAxis = Vector2.zero;
